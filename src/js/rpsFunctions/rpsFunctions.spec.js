9a8832954a309ed327df28f2ecc3477f48aa77f6
Adam Chilton <adam@dsan.co.uk>
2020-04-06 12:27:47 +0100

    adds winner test

--- src/js/rpsFunctions/rpsFunctions.spec.js
+++ src/js/rpsFunctions/rpsFunctions.spec.js
@@ -1,6 +1,6 @@
-import { computerChooses } from './rpsFunctions'
+import { computerChooses, winner } from './rpsFunctions'
 
-describe('computerChooses', () => {
+describe('computerChooses()', () => {
 	it('should select a number between 0 and 2', () => {
 		const value = computerChooses(3)
 		expect(value).toBeGreaterThan(-1)
@@ -12,5 +12,35 @@ describe('computerChooses', () => {
 		expect(value).toBeGreaterThan(-1)
 		expect(value).toBeLessThan(5)
 		expect(Number.isInteger(value)).toBeTruthy()
+	})
+})
+
+describe('winner()', () => {
+	it('should state draw', () => {
+		const output = 'draw'
+		const scissor = winner(2, 2)
+		const paper = winner(1, 1)
+		const rock = winner(0, 0)
+		expect(rock).toEqual(output)
+		expect(paper).toEqual(output)
+		expect(scissor).toEqual(output)
+	})
+	it('should state p1 wins', () => {
+		const output = 'player 1 wins'
+		const rock = winner(0, 2)
+		const paper = winner(1, 0)
+		const scissor = winner(2, 1)
+		expect(rock).toEqual(output)
+		expect(paper).toEqual(output)
+		expect(scissor).toEqual(output)
+	})
+	it('should state p2 wins', () => {
+		const output = 'player 2 wins'
+		const rock = winner(2, 0)
+		const paper = winner(0, 1)
+		const scissor = winner(1, 2)
+		expect(rock).toEqual(output)
+		expect(paper).toEqual(output)
+		expect(scissor).toEqual(output)
 	})
 })
